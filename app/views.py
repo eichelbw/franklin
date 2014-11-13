bf9d644cd3e8421178b2484d14dac1d3dedcffa6
W Eich <rw.eichelberger@gmail.com>
2014-11-13 15:54:34 -0800

    add user profile route

--- app/views.py
+++ app/views.py
@@ -45,6 +45,22 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    """returns profile page for user"""
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+            {'author': user, 'body': 'Test post 1'},
+            {'author': user, 'body': 'Test post 2'}
+            ]
+    return render_template('user.html',
+            user=user,
+            posts=posts)
+
 @app.before_request
 def before_request():
     g.user = current_user
