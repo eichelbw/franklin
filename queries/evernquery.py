23166dce5ab59eae88117d16c61f31fa6d9b196e
W Eich <rw.eichelberger@gmail.com>
2014-11-14 22:17:45 -0800

    create a note (confirmed to work)

--- queries/evernquery.py
+++ queries/evernquery.py
@@ -1,4 +1,5 @@
 from evernote.api.client import EvernoteClient
+import evernote.edam.type.ttypes as Types
 
 dev_token = "S=s385:U=3e7d2ff:E=150f8d4c12f:C=149a12394c0:P=1cd:A=en-devtoken:V=2:H=44734894e812cbfe03d83cc365d8c9c5"
 client = EvernoteClient(token=dev_token, sandbox=False)
@@ -10,3 +11,8 @@ noteStore = client.get_note_store()
 notebooks = noteStore.listNotebooks()
 for n in notebooks:
     print n.name
+
+note = Types.Note()
+note.title = "test note"
+note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>Hello world!</en-note>'
+note = noteStore.createNote(note)
