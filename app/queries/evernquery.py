cfcbabddb71ab2e76b15d1fb9e64295f90e25c9d
W Eich <rw.eichelberger@gmail.com>
2014-11-17 15:52:37 -0800

    no working changes, just blocking out thoughts

--- app/queries/evernquery.py
+++ app/queries/evernquery.py
@@ -1,5 +1,6 @@
 from evernote.api.client import EvernoteClient
 from evernote.edam.notestore.ttypes import NoteFilter, NotesMetadataResultSpec
+import evernote.edam.type.ttypes as Types
 from evernote.edam.type.ttypes import NoteSortOrder
 
 dev_token = "S=s385:U=3e7d2ff:E=150f8d4c12f:C=149a12394c0:P=1cd:A=en-devtoken:V=2:H=44734894e812cbfe03d83cc365d8c9c5"
@@ -20,12 +21,29 @@ def get_todo_notes():
     """executes EN api query and returns content of note tagged 'todo'"""
     result_list = note_store.findNotesMetadata(dev_token, note_filter, offset, max_notes, result_spec)
     todo_note_guid = result_list.notes[0].guid
-    #todo_content = note_store.getNote(dev_token, str(todo_note_guid), True, False, False, False).content
     return note_store.getNote(dev_token, str(todo_note_guid), True, False, False, False)
 
 def set_todo_content(updated_note):
     """updates EN api with modifications to updated_note"""
-    note_store.updateNote(updated_note)
+    # updateNote() needs three things:
+    # 1) todo note's guid
+    # 2) todo note's title
+    # 3) todo note's full content with change.
+    # ---
+    # things in the way currently:
+    # 1) not sure if i should create an object in this method to pass to updateNote and assign attributes
+    #    directly from the earlier note from get_todo_notes() or to update previous object in jquery and
+    #    pass it back to flask.
+    # for example, the following series of commands works in terminal:
+    # tdn = evernquery.get_todo_notes()
+    # upnote = Types.Note()
+    # upnote.title, upnote.guid = tdn.title, tdn.guid
+    # upnote.content = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">\n<en-note><div>Hello world!</div><div><br clear="none"/></div><div>it\'s different now! even more so!</div><div><br clear="none"/></div><div><en-todo checked="true"></en-todo>learn how to interact with todos</div><div><en-todo checked="true"></en-todo>second one for testing</div></en-note>'
+    # note_store.updateNote(upnote)
+
+###############
+### TESTING ###
+###############
 
 #note = Types.Note()
 #note.title = "test note"
